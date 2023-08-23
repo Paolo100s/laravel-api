59689c1262cbe8aad5dfde811cf998165ac87a6a
PaoloCentoscudi <paolocentoscudi@gmail.com>
2023-08-23 09:26:00 +0200

    add ProjectController

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\ProjectController;
+
+Route::prefix('z0')->group (function(){
+    Route::get('/projects', [ProjectController::class, 'projectIndex']);
+});
